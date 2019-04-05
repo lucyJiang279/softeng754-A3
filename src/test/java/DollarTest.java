e25bb38f0c7c1e0df245c58598bb050f4fd32b22
lucyJiang279 <ljia374@aucklanduni.ac.nz>
2019-04-05 14:12:55 +1300

    added test file

---
+++ src/test/java/DollarTest.java
@@ -0,0 +1,27 @@
+import static org.junit.Assert.*;
+//import org.bson.Document;
+import org.junit.Test;
+//import com.mongodb.MongoClient;
+//import com.mongodb.client.MongoCollection;
+//import com.mongodb.client.MongoDatabase;
+
+public class DollarTest {
+
+    @Test
+    public void testDollarDivision() {
+        // Given
+        Dollar ten = new Dollar(10);
+        // When
+        Dollar result = ten.dividedBy(2);
+        // Then
+        assertEquals(new Dollar(5), result); // You could change 5 to 4 to see the build failure
+    }
+
+    @Test(expected=ArithmeticException.class)
+    public void testDollarDividedByZero() {
+        // Given
+        Dollar ten = new Dollar(10);
+        // When
+        ten.dividedBy(0);
+    }
+}
